e4b62fefc56723bbe044e027ed4819e6d852e880
Abel Herrera <ahpard97@gmail.com>
2023-01-29 20:29:10 -0600

    add test returned value

--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,10 @@ describe('getAreas test suite', () => {
    it('should not throw an error executing function', () => {
       expect(() => getAreas([], [])).not.toThrow();
    });
+
+   it('should return a promise', () => {
+      const returnedValue = getAreas([], []);
+
+      expect(returnedValue instanceof Promise).toBeTruthy();
+   });
 });
