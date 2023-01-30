25472e79f10f5f1503945421c8d73c6cf2ee0767
Abel Herrera <ahpard97@gmail.com>
2023-01-29 20:29:36 -0600

    add test valid areas calculation

--- src/index.test.js
+++ src/index.test.js
@@ -10,4 +10,19 @@ describe('getAreas test suite', () => {
 
       expect(returnedValue instanceof Promise).toBeTruthy();
    });
+
+   it('should return areas correctly', () => {
+      getAreas(['square', 'rectangle', 'circle', 'triangle'], [[2], [3, 4], [5], [2, 4]]).then(
+         (areas) => {
+            expect(areas).toEqual([4, 12, 78.5, 4]);
+         }
+      );
+
+      getAreas(
+         ['square', 'square', 'rectangle', 'circle', 'triangle'],
+         [[2], [4], [5, 6], [8], [3, 7]]
+      ).then((areas) => {
+         expect(areas).toEqual([4, 16, 30, 200.96, 10.5]);
+      });
+   });
 });
